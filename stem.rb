2d508a37c565bc386e5e8db9098ce080370400d3
Peter van Hardenberg <pvh@heroku.com>
2010-06-23 14:34:00 -0700

    select AZ from config.json, and use a generic blank snapshot to create the volumes

--- stem.rb
+++ stem.rb
@@ -8,7 +8,9 @@ CONFIG = ARGV[0]
 USERDATA = ARGV[1]
 
 config = JSON.parse File.read ARGV[0]
-throw "No config"
+throw "No config" unless config
+
+avail_zone = config["availability_zone"] || "us-east-1c"
 
 ebs = config["volumes"].select {|v| v["media"] == "ebs"}
 
@@ -16,22 +18,34 @@ ebs = config["volumes"].select {|v| v["media"] == "ebs"}
 
 snapshots = []
 devices = []
+sizes = []
 ebs.each do |v|
-  volumeId = c.call("CreateVolume", "Size" => v["size"].to_s,  "AvailabilityZone" => "us-east-1b")["volumeId"]
-  snapshotId = c.call("CreateSnapshot", "VolumeId" => volumeId)["snapshotId"]
-  snapshots << snapshotId
+  puts "Adding a volume of #{v["size"]} to be mounted at #{v["device"]}."
+#  volumeId = c.call("CreateVolume", "Size" => v["size"].to_s,  "AvailabilityZone" => avail_zone)["volumeId"]
+#  snapshotId = c.call("CreateSnapshot", "VolumeId" => volumeId)["snapshotId"]
+#  snapshots << snapshotId
+  snapshots << "snap-87fcd7ef" # an empty snapshot
   devices << v["device"]
+  sizes << v["size"].to_s
 end
 
 opt = {
+  "Placement.AvailabilityZone" => avail_zone,
   "MinCount" => "1",
   "MaxCount" => "1",
   "KeyName" => "default",
   "ImageId" => config["ami32"],
   "BlockDeviceMapping.#.Ebs.SnapshotId" => snapshots,
+  "BlockDeviceMapping.#.Ebs.VolumeSize" => sizes,
   "BlockDeviceMapping.#.DeviceName" => devices
 }
-opt.merge({ "UserData" => Base64.encode64( File.read(USERDATA) )}) if File.read(USERDATA)
+
+if USERDATA && data = File.read(USERDATA)
+  puts "Including userdata."
+  opt.merge!({ "UserData" => Base64.encode64(data)})
+end
+
+pp opt
 
 pp c.call "RunInstances", opt
 
