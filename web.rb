6d4ee527d9f1cb7558816d9c8f7dd28cb8671128
Peter van Hardenberg <pvh@heroku.com>
2010-06-24 21:05:12 -0700

    working db provisioning

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'json'
 require 'lib/greenfield'
+require 'mustache'
 
 module GreenField
   class HerokuApi < Sinatra::Base
@@ -11,18 +12,32 @@ module GreenField
       callback_url = params["callback_url"]
 
       config = JSON.parse( File.read("postgres-server/config.json") )
-      userdata = File.read("postgres-server/userdata.sh")
+      template = File.read("postgres-server/userdata.sh.mustache")
+      data = {
+        "db_role" => (10 + rand(26)).to_s(36) + rand(2**64).to_s(36),
+        "db_password" => rand(2**128).to_s(36),
+        "pg_admin_password" => rand(2**128).to_s(36)
+      }
+      userdata = Mustache.render(template, data)
 
+      puts "Allocating an IP"
       ip = GreenField.allocate_ip
       instance = GreenField.create(config, userdata)
 
-      while GreenField.inspect(instance)["instanceState"]["name"] != "running"
+      require 'pp'
+      pp GreenField.inspect(instance)
+
+      while true
+        i = GreenField.inspect(instance)
+        break if i["instanceState"]["name"] == "running"
         sleep 1
+        puts "Waiting... #{ i["instanceState"]["name"] }"
       end
 
       GreenField.associate_ip(ip, instance)
 
-      {:id => id, :config => {"GREENFIELD_URL" => "http://#{ip}"}}.to_json
+      {:id => id, :config => {"GREENFIELD_URL" =>
+        "postgres://#{data["db_role"]}:#{data["db_password"]}#{ip}/#{data["db_role"]}"}}.to_json
     end
 
     delete "/resources/:name" do |name|
