f36cc2e003b0e730bb956e3dc28aa5933a299f46
Peter van Hardenberg <pvh@heroku.com>
2010-06-28 18:19:14 -0700

    attempt to expand kuzushi out of existence and eliminate the requirement for the config.json on the server

--- chrysanthemum/postgres-prototype/userdata.sh
+++ chrysanthemum/postgres-prototype/userdata.sh
@@ -10,58 +10,30 @@ echo "--- BEGIN"
 export DEBIAN_FRONTEND="noninteractive"
 export DEBIAN_PRIORITY="critical"
 
-# you only need this part if you're using volumes
-echo "--- KUZUSHI"
-apt-get update
-apt-get install ruby rubygems ruby-dev irb libopenssl-ruby libreadline-ruby curl -y
-gem install kuzushi --no-rdoc --no-ri
-echo 'export PATH=`ruby -r rubygems -e "puts Gem.bindir"`:$PATH' >> /etc/profile ; . /etc/profile
-
-# gotta get kuzushi the config.json
-apt-get -y install s3cmd
-
-# ugggg
-cat > config.json <<CONFIG
-{
-  "ami32":"ami-714ba518", // public ubuntu 10.04 ami - 32 bit
-  "availability_zone" : "us-east-1a",
-
-  "volumes" : [
-    { "device" : "/dev/sde1", "media" : "ebs", "size" : 64, "format" : "ext3", "scheduler" : "deadline", "label" : "/wal", "mount": "/wal", "mount_options" : "nodev,nosuid,noatime" },
-    { "device" : "/dev/md0",
-      "media" : "raid",
-      "label" : "/database",
-      "mount" : "/database",
-      "drives" : [ "/dev/sdf1", "/dev/sdf2", "/dev/sdf3", "/dev/sdf4", "/dev/sdf5", "/dev/sdf6", "/dev/sdf7", "/dev/sdf8" ],
-      "level" : 0,
-      "chunksize" : 256,
-      "readahead" : 65536,
-      "format" : "xfs"  // implies xfsprogs package
-    }
-   ]
-}
-CONFIG
-
-export JUDO_FIRST_BOOT=true
-kuzushi-setup
-
 echo "--- CONFIGURE DRIVES"
-service udev stop
-mdadm --create /dev/md0 -n 8 -l 0 -c 256 /dev/sdf{1..8}
-service udev start
-blockdev --setra 65535 /dev/md0
+apt-get install -y mdadm xfsprogs
 
-# this should be part of kuzushi
-mdadm -Es >>/etc/mdadm/mdadm.conf
-echo "/dev/md0          /database   xfs" >> /etc/fstab
+echo "   --- WAL"
+echo deadline > /sys/block/sde1/queue/scheduler
+mkfs.ext3 -q -L /wal /dev/sde1
+mkdir -p /wal && mount -o noatime -t ext3 /dev/sde1 /wal
 echo "/dev/sde1         /wal        ext3   nodev,nosuid,noatime" >> /etc/fstab
 
-# this should be done every boot
+
+echo "   --- CREATE RAID" 
 for i in /sys/block/sdf{1..8}/queue/scheduler; do
   echo "deadline" > $i
 done
+service udev stop
+mdadm --create /dev/md0 -n 8 -l 0 -c 256 /dev/sdf{1..8}
+mdadm -Es >>/etc/mdadm/mdadm.conf
+service udev start
+blockdev --setra 65535 /dev/md0
 
-
+echo "   --- FORMAT RAID AND MOUNT"
+mkfs.xfs -q -L /database /dev/md0
+mkdir -p /database && mount -o noatime -t xfs /dev/md0 /database
+echo "/dev/md0          /database   xfs" >> /etc/fstab
 
 echo "--- POSTGRESQL INSTALL"
 apt-get -y install thin postgresql-8.4 postgresql-server-dev-8.4 libpq-dev libgeos-dev proj
