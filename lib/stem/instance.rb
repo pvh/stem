934e8b70029c7808d63b98794ad9e5d8615b6e90
Peter van Hardenberg <pvh@heroku.com>
2010-08-04 14:36:08 -0700

    image named renamed

--- lib/stem/instance.rb
+++ lib/stem/instance.rb
@@ -9,7 +9,7 @@ module Stem
       if config["ami"]
         ami = config["ami"]
       elsif config["ami-name"]
-        ami = image_named(config["ami-name"])
+        ami = Image::named(config["ami-name"])
         throw "AMI named #{config["ami-name"]} was not found. (Does it need creating?)" unless ami
       end
       throw "No AMI specified." unless ami
