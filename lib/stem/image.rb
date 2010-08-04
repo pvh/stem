6089f9239a8de37b1717fcccfe830bff2990faa9
Peter van Hardenberg <pvh@heroku.com>
2010-08-04 13:46:49 -0700

    port over shogun to new stem style

--- lib/stem/image.rb
+++ lib/stem/image.rb
@@ -2,22 +2,22 @@ module Stem
   module Image
     extend self
 
-    def capture name, instance
-      description = {} # more to come here...
+    def snapshot name, instance
+      description = {}
       swirl.call("CreateImage", "InstanceId" => instance, "Name" => name, "Description" => "%%" + description.to_json)["imageId"]
     end
 
-    def image_named name
+    def release image
+      swirl.call("DeregisterImage", "ImageId" => image)["return"]
+    end
+
+    def named name
         i = swirl.call "DescribeImages", "Owner" => "self"
         ami = i["imagesSet"].select {|m| m["name"] == name }.map { |m| m["imageId"] }.first
     end
 
-    def describe_image image
+    def describe image
       swirl.call("DescribeImages", "ImageId" => image)["imagesSet"][0]
-    end
-
-    def deregister_image image
-      swirl.call("DeregisterImage", "ImageId" => image)["return"]
     end
   end
 end
