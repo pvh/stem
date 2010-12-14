330d2f8feabf502adc8d2dfdc230ef0a755e10f8
Peter van Hardenberg <pvh@heroku.com>
2010-12-14 00:49:48 -0800

    stem gem bug bump

--- lib/stem/instance.rb
+++ lib/stem/instance.rb
@@ -13,7 +13,7 @@ module Stem
         ami = Image::named(config["ami-name"])
         throw "AMI named #{config["ami-name"]} was not found. (Does it need creating?)" unless ami
       elsif config["ami-tags"]
-        ami = Image::tagged(config['ami-tags'])
+        ami = Image::tagged(config['ami-tags'])[0]
         throw "AMI tagged with #{config['ami-tags'].inspect} was not found. (Does it need creating?)" unless ami
       end
       throw "No AMI specified." unless ami
