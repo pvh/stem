c0aff91664f32b53f6610b987e6a1f88c73b25bc
Blake Gentry <blakesgentry@gmail.com>
2011-02-07 10:11:21 -0800

    Add sleep(1) to spec to ensure compilations are done w/ different timestamps

--- spec/userdata_spec.rb
+++ spec/userdata_spec.rb
@@ -4,8 +4,9 @@ require 'lib/stem'
 describe Stem::Userdata do
   describe '#compile' do
     it "should produce identical output with identical input" do
-      output1 = Stem::Userdata.compile("spec/fixtures/userdata")
-      output2 = Stem::Userdata.compile("spec/fixtures/userdata")
+      output1 = Stem::Userdata.compile("spec/fixtures/userdata", :erb_binding => binding)
+      sleep(1)  # Necessary to ensure different timestamps
+      output2 = Stem::Userdata.compile("spec/fixtures/userdata", :erb_binding => binding)
       output1.should == output2
     end
   end
