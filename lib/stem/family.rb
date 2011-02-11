04a091c078bab32215b67968eb897700eb8bcfed
Peter van Hardenberg <pvh@heroku.com>
2011-02-10 18:25:00 -0800

    wip: build_image

--- lib/stem/family.rb
+++ lib/stem/family.rb
@@ -1,3 +1,5 @@
+require 'sha1'
+
 module Stem
   module Family
     extend self
@@ -27,6 +29,64 @@ module Stem
       end
       unrelease family, release_name
       amis.each { |ami| Stem::Tag::create(ami, :release => release_name) }
+    end
+
+    def build_image(family, config, userdata)
+      log = lambda { |msg|
+        puts "[#{family}|#{Time.now.to_s}] #{msg}"
+      }
+
+      log.call "Beginning to build image for #{family}"
+      log.call "Config:\n------\n#{ config.inspect }\n-------"
+      instance_id = Stem::Instance.launch(config, userdata)
+
+      log.call "Booting #{instance_id} to produce your prototype instance"
+      wait_for_stopped instance_id, log
+
+      build_hash = SHA1::hexdigest( userdata + config.to_s )
+      image_id = Stem::Image.create("#{family}-#{build_hash}",
+                                    instance_id,
+                                    {
+                                      :family => family,
+                                      :created => Time.now.utc.iso_8601
+                                    })
+      log.call "Image ID is #{image_id}"
+
+      wait_for_available(image_id, log)
+
+      log "Terminating #{instance_id} now that the image is captured"
+      Stem::Instance::terminate(instance_id)
+    end
+
+    protected
+
+    def wait_for_stopped(instance_id, log)
+      log.call "waiting for instance to reach state stopped -- "
+      while sleep 10
+        state = Stem::Instance.describe(instance_id)["instanceState"]["name"]
+        log.call "instance #{instance_id} #{state}"
+        break if state == "stopped"
+      end
+    end
+
+    def wait_for_available(image_id, log)
+      log.call "waiting for image to finish capturing..."
+      while sleep 10
+        begin
+          state = Stem::Image.describe(image_id)["imageState"]
+          log.call "image #{image_id} #{state}"
+          if state == "available"
+            log.call "image capturing succeeded"
+            break
+          elsif state == "pending"
+            # continue
+          else
+            throw "image unexpectedly entered #{state}"
+          end
+        rescue Swirl::InvalidRequest => e
+          raise unless e.message =~ /does not exist/
+        end
+      end
     end
   end
 end
