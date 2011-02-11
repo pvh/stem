abd947ec440d52b2e037ea6b9bcebaaca6bae1d2
Peter van Hardenberg <pvh@heroku.com>
2011-02-10 16:55:46 -0800

    actually, amazon tags are just hashes after all

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -1,9 +1,11 @@
 require 'lib/stem/util'
 include Stem::Util
 
-describe "tag support utilities" do
+describe "tagset_to_hash" do
   it "returns a hash when fed a stupid tagset" do
-    tagset = {"item"=>[{"value"=>"postgres9-server", "key"=>"type"}, {"value"=>"production", "key"=>"version"}]}
+    tagset = {"item"=>[
+      {"value"=>"postgres9-server", "key"=>"type"},
+      {"value"=>"production", "key"=>"version"}]}
     tagset_to_hash(tagset).should == {"type" => "postgres9-server", "version" => "production"}
   end
 
@@ -11,16 +13,40 @@ describe "tag support utilities" do
     tagset = {"item"=>[{"key" => "deprecated"}]}
     tagset_to_hash(tagset).should == {"deprecated" => nil}
   end
+end
 
-  it "returns an array for multiple tags" do
-    tagset = {"item"=>[ {"value"=>"staging", "key" => "version"}, {"value"=>"production", "key"=>"version"}]}
-    # yeah, this test result is a little fragile, since the order of the results is arbitrary
-    tagset_to_hash(tagset).should == { "version" => ["staging", "production"] }
+describe "filter_opts" do
+  it "translates a hash into amazon FilterOpts" do
+    tags = {"tag:version" => "production",
+            "tag:family" => "postgres9-server"}
+
+    get_filter_opts(tags).should == { "Filter.1.Value.0"=>"production",
+                                      "Filter.0.Value.0"=>"postgres9-server",
+                                      "Filter.1.Name"=>"tag:version",
+                                      "Filter.0.Name"=>"tag:family" }
   end
 
-  it "supports three or more multiple tags for a key" do
-    tagset = {"item"=>[ {"value"=>"staging", "key" => "version"}, {"value"=>"production", "key"=>"version"}, {"value" => "testing", "key" => "version"}]}
-    # yeah, this test result is a little fragile, since the order of the results is arbitrary
-    tagset_to_hash(tagset).should == { "version" => ["staging", "production", "testing"] }
+  it "supports multiple values for the query" do
+    tags = {"tag:version" => "production",
+            "tag:family" => ["postgres84-server", "postgres9-server"]}
+
+    get_filter_opts(tags).should == { "Filter.1.Value.0"=>"production",
+                                      "Filter.0.Value.0"=>"postgres84-server",
+                                      "Filter.0.Value.1"=>"postgres9-server",
+                                      "Filter.1.Name"=>"tag:version",
+                                      "Filter.0.Name"=>"tag:family" }
+  end
+end
+
+describe "tags_to_filter" do
+  it "translates tags into the aws style" do
+    tags = {:version => "production",
+             :family => "postgres9-server"}
+    tags_to_filter(tags).should == get_filter_opts({"tag:version" => "production",
+                                    "tag:family" => "postgres9-server"})
+  end
+  it "special-cases architecture tag" do
+    tags = {:architecture => "i386"}
+    tags_to_filter(tags).should == get_filter_opts({"architecture" => "i386"})
   end
 end
