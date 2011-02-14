aa313575ff8e79ace5ec0bc88e1302c250c4b253
Blake Gentry <blakesgentry@gmail.com>
2011-02-13 21:22:19 -0800

    Additional specs for Stem::Image.tagged

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -7,10 +7,21 @@ describe Stem::Image do
 
     it { should respond_to :tagged }
 
+    it "should return nil if an empty hash is passed in" do
+      Stem::Image.tagged([]).should be_nil
+    end
+
+    it "should return nil if an empty array is passed in" do
+      Stem::Image.tagged([]).should be_nil
+    end
+
     it "should return an empty array when no images exist with the specified tags" do
       Stem::Image.tagged(:faketag => 'does_not_exist').should == []
     end
 
+    it "should retun an array of ami IDs when 2 images exist with the specified tags" do
+      Stem::Image.tagged(:slot => 'logplex', :architecture => 'x86_64')
+    end
   end
 end
 
