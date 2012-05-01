f4e5128a36906e9068c45df8f8244e3737019569
Will Leinweber <will@bitfission.com>
2012-05-01 15:36:01 -0700

    add m1.medium and change all defaults to 64bit

--- lib/stem/instance_types.rb
+++ lib/stem/instance_types.rb
@@ -2,13 +2,15 @@ module Stem
   module Instance
     Types =
       {"m1.small" => {
-        "architecture" => "i386"},
+        "architecture" => "x86_64"},
+       "m1.medium" => {
+        "architecture" => "x86_64"},
        "m1.large" => {
         "architecture" => "x86_64"},
        "m1.xlarge" => {
         "architecture" => "x86_64"},
        "t1.micro" => {
-        "architecture" => "i386"},
+        "architecture" => "x86_64"},
        "m2.xlarge" => {
         "architecture" => "x86_64"},
        "m2.2xlarge" => {
@@ -16,7 +18,7 @@ module Stem
        "m2.4xlarge" => {
         "architecture" => "x86_64"},
        "c1.medium" => {
-        "architecture" => "i386"},
+        "architecture" => "x86_64"},
        "c1.xlarge" => {
         "architecture" => "x86_64"},
        "cc1.4xlarge" => {
